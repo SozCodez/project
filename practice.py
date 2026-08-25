1a17d559b4e03c937fc1c69e78a28307610dfaff
StudentPM <studentpm@Administrators-MacBook-Pro.local>
2026-08-25 13:39:30 -0700

    making python code file

---
+++ practice.py
@@ -0,0 +1,10 @@
+#This is a comment!
+#Send to Dagestan 2-3 years and forget.
+"""random comment
+random comment
+random comment
+randomcommentrandomcomment
+random comment
+random comment random comment
+randomcomment
+rand comment"""
